30de6ca5ecba83066fc1c23613de11d5e0c1730f
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-05-26 17:28:14 +0530

    Update 01. Minimum bit flips.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 06.Bit Manipulation/2. Interview Problems/01. Minimum bit flips.cpp
+++ 06.Bit Manipulation/2. Interview Problems/01. Minimum bit flips.cpp
@@ -32,3 +32,19 @@ int minBitFlips(int start, int goal) {
 // Space Complexity: O(1)
 
 
+// optimized
+int minBitFlips(int start, int goal) {
+    int x = start ^ goal;
+    int cnt = 0;
+
+    while (x) {
+        cnt += (x & 1);
+        x >>= 1;
+    }
+
+    return cnt;
+}
+
+
+
+
