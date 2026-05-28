369b32e600848df4e59c76dffac90dfed558a0a2
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-05-28 16:53:18 +0530

    Update 02. Print LIS.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/7. DP on LIS/02. Print LIS.cpp
+++ 14. Dynamic Programming/7. DP on LIS/02. Print LIS.cpp
@@ -2,16 +2,6 @@
 QUESTION:
 Given an integer n and an array of integers, return the Longest Increasing Subsequence which is lexicographically smallest corresponding to the indices of the elements.
 
-APPROACH:
-- This problem can be solved using dynamic programming.
-- Let dp[i] represent the length of the longest increasing subsequence ending at index i.
-- Additionally, maintain a prevmap to track the previous index in the increasing subsequence.
-- Iterate through the array and for each index i, iterate through all previous indices (prev) from 0 to i-1.
-- If arr[prev] < arr[i] and dp[i] < dp[prev] + 1, update dp[i] and prevmap[i].
-- Find the index i where dp has the maximum value.
-- Construct the Longest Increasing Subsequence using the prevmap and the found index i.
-- Return the LIS in reverse order to get the lexicographically smallest sequence.
-
 COMPLEXITY ANALYSIS:
 - The time complexity of this approach is O(n^2), where n is the length of the input array arr.
 - The space complexity is O(n) due to the dp and prevmap arrays, as well as the lis vector.
@@ -21,31 +11,54 @@ CODE:
 */
 
 // Here, we use 1D dp vector and while loop helps us to push the elements into ans vector and we reverse it at the end
-vector<int> ftab(int n, vector<int>& arr) {
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> printLIS(vector<int>& nums) {
+    int n = nums.size();
+
     vector<int> dp(n, 1);
-    vector<int> prevmap(n, -1);
-    
+    vector<int> parent(n);
+
+    // initialize parent pointers
+    for (int i = 0; i < n; i++) parent[i] = i;
+
+    int maxLen = 1;
+    int lastIndex = 0;
+
     for (int i = 0; i < n; i++) {
-        for (int prev = 0; prev < i; prev++) {
-            if (arr[prev] < arr[i] && dp[i] < dp[prev] + 1) {
-                dp[i] = dp[prev] + 1;
-                prevmap[i] = prev;
+        for (int j = 0; j < i; j++) {
+            if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
+                dp[i] = dp[j] + 1;
+                parent[i] = j;   // store previous element
             }
         }
+
+        if (dp[i] > maxLen) {
+            maxLen = dp[i];
+            lastIndex = i;
+        }
     }
-    
-    int i = max_element(dp.begin(), dp.end()) - dp.begin();
+
+    // reconstruct LIS
     vector<int> lis;
-    
-    while (i >= 0) {
-        lis.push_back(arr[i]);
-        i = prevmap[i];
+    int i = lastIndex;
+
+    while (parent[i] != i) {
+        lis.push_back(nums[i]);
+        i = parent[i];
     }
-    
+    lis.push_back(nums[i]);
+
     reverse(lis.begin(), lis.end());
     return lis;
 }
 
-vector<int> longestIncreasingSubsequence(int n, vector<int>& arr) {
-    return ftab(n, arr);
+int main() {
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    vector<int> lis = printLIS(nums);
+    for (int x : lis) {
+        cout << x << " ";
+    }
 }
