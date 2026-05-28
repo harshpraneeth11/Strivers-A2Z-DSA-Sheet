60665b88c0d950312cfd013aebcdb581762dff8c
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-05-28 16:54:25 +0530

    Update and rename 06. LIS using Binary search to 06. LIS using Binary search.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/7. DP on LIS/06. LIS using Binary search.cpp
+++ 14. Dynamic Programming/7. DP on LIS/06. LIS using Binary search.cpp
@@ -10,25 +10,27 @@ If 4 is there, replace 4 with 4
 */
 
 // Function to find the length of the longest increasing subsequence
-int longestIncreasingSubsequence(int arr[], int n) {
-    // Create a temporary vector to store the increasing subsequence
-    vector<int> temp;
-    temp.push_back(arr[0]);
 
-    int len = 1;
+#include <bits/stdc++.h>
+using namespace std;
 
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > temp.back()) {
-            // If arr[i] is greater than the last element of temp, extend the subsequence
-            temp.push_back(arr[i]);
-            len++;
+int lengthOfLIS(vector<int>& nums) {
+    vector<int> tail;
+
+    for (int x : nums) {
+        auto it = lower_bound(tail.begin(), tail.end(), x);
+
+        if (it == tail.end()) {
+            tail.push_back(x);
         } else {
-            // If arr[i] is not greater, replace the element in temp with arr[i]
-            int ind = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
-            temp[ind] = arr[i];
+            *it = x;
         }
     }
 
-    return len;
+    return tail.size();
 }
 
+int main() {
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    cout << lengthOfLIS(nums);
+}
