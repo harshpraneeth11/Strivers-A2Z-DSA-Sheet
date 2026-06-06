1b19e65b62dce7ed3df8fb74440d60564f9c8150
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-06 17:51:37 +0530

    Update 13.Find_how_many_times_array_is_rotated.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 02.Binary Search/1D Arrays/13.Find_how_many_times_array_is_rotated.cpp
+++ 02.Binary Search/1D Arrays/13.Find_how_many_times_array_is_rotated.cpp
@@ -30,7 +30,7 @@ int findKRotation(int arr[], int n) {
     int low = 0, high = n - 1;
     while (low < high) {
         int mid = low + (high - low) / 2;
-        if (arr[mid] > arr[n - 1])
+        if (arr[mid] > arr[high])
             low = mid + 1;
         else
             high = mid;
