38aae3c41a9facaa107eedc747ac6b153b2760f5
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-08 20:52:22 +0530

    Update 09.Search_in_rotated_sorted_array.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 02.Binary Search/1D Arrays/09.Search_in_rotated_sorted_array.cpp
+++ 02.Binary Search/1D Arrays/09.Search_in_rotated_sorted_array.cpp
@@ -38,22 +38,30 @@ CODE:
 
 int search(vector<int>& nums, int target) {
     int low = 0, high = nums.size() - 1;
+
     while (low <= high) {
         int mid = low + (high - low) / 2;
+        // target found
         if (nums[mid] == target)
             return mid;
+        // check if left half is sorted
         if (nums[low] <= nums[mid]) {
+            // target lies in sorted left half
             if (nums[low] <= target && target <= nums[mid])
                 high = mid - 1;
             else
                 low = mid + 1;
-        } else {
+        } 
+        // right half is sorted
+        else {
+            // target lies in sorted right half
             if (nums[mid] <= target && target <= nums[high])
                 low = mid + 1;
             else
                 high = mid - 1;
         }
     }
+    // target not found
     return -1;
 }
 
