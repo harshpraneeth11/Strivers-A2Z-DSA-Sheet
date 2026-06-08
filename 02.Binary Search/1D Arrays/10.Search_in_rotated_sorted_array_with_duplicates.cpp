f402e1beddd9c09649a7177b4bbb3273544640da
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-08 20:57:39 +0530

    Update 10.Search_in_rotated_sorted_array_with_duplicates.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 02.Binary Search/1D Arrays/10.Search_in_rotated_sorted_array_with_duplicates.cpp
+++ 02.Binary Search/1D Arrays/10.Search_in_rotated_sorted_array_with_duplicates.cpp
@@ -36,27 +36,42 @@ CODE:
 
 bool search(vector<int>& nums, int target) {
     int low = 0, high = nums.size() - 1;
+
     while (low <= high) {
         int mid = low + (high - low) / 2;
+
+        // found target
         if (nums[mid] == target)
             return true;
+
+        // remove ambiguity due to duplicates
+        // We can use while loop with condition of (low < high && ...) in it
+        // But not recommended as it is aggressive
         if (nums[mid] == nums[low] && nums[mid] == nums[high]) {
             low++;
             high--;
             continue;
         }
+
+        // left half is sorted
         if (nums[low] <= nums[mid]) {
+            // target in left half
             if (nums[low] <= target && target <= nums[mid])
                 high = mid - 1;
             else
                 low = mid + 1;
-        } else {
+        } 
+        // right half is sorted
+        else {
+            // target in right half
             if (nums[mid] <= target && target <= nums[high])
                 low = mid + 1;
             else
                 high = mid - 1;
         }
     }
+
+    // not found
     return false;
 }
 
