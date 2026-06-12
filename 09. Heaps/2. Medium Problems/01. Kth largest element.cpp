950a5a7007a885fc09924324eddad08714d4a22d
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-12 19:45:38 +0530

    Update 01. Kth largest element.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 09. Heaps/2. Medium Problems/01. Kth largest element.cpp
+++ 09. Heaps/2. Medium Problems/01. Kth largest element.cpp
@@ -1,40 +1,48 @@
 /*
 QUESTION:
 Given an integer array nums and an integer k, return the kth largest element in the array.
-
-APPROACH:
-To find the kth largest element in the array, we can use a min-heap of size k.
-1. Initialize a min-heap (priority queue) of size k.
-2. Insert the first k elements of the array into the min-heap.
-3. For the remaining elements in the array, if an element is larger than the top element of the min-heap, replace the top element with the current element and heapify to maintain the min-heap property.
-4. After processing all the elements, the top element of the min-heap will be the kth largest element in the array.
-
-The findKthLargest function implements this approach. It uses a min-heap (priority queue) to find the kth largest element in the array without sorting the entire array.
-
-TIME COMPLEXITY:
-The time complexity of the findKthLargest function is O(N log k), where N is the size of the array. Inserting elements into the min-heap and heapifying take O(log k) time, and we do this for N-k elements. The overall time complexity is dominated by the heap operations.
-
-SPACE COMPLEXITY:
-The space complexity is O(k) as we need to store k elements in the min-heap.
-
-** Keeping a min Heap and if we keep removing the top element (if a[i] > st.top) then at the end,
-we get k elements which are largest of all and the top has, kth largest
-
-** We use minHeap, because at the end, the 1st largest stays at bottom and 
-at the top will be kth largest
-
 */
 
+// Approach 1 : minHeap means small at top. push elements one by one and keep only k in the PQ
+// Approach 2 : maxHeap means large at top. push all elements and remove k-1, so, the kth largest will be at top.
+
+// Time: O(n log k)  -  as stack contains k elements at each position
+// Space: O(k)
+
+Push elements ony by one into minHeap
+If the size > k, pop one by one
+So, at the end we get kth largest at top and 1st largest at bottom
+
 int findKthLargest(vector<int>& nums, int k) {
-    priority_queue<int, vector<int>, greater<int>> pq;
-    for (int i = 0; i < k; i++) {
-        pq.push(nums[i]);
-    }
-    for (int i = k; i < nums.size(); i++) {
-        if (nums[i] > pq.top()) {
+    priority_queue<int, vector<int>, greater<int>> pq; // min heap
+
+    for (int num : nums) {
+        pq.push(num);
+
+        if (pq.size() > k) {
             pq.pop();
-            pq.push(nums[i]);
         }
+    }
+    return pq.top();
+}
+
+
+Push all elements into n elements in the maxHeap
+Pop the k-1 elements, here top is max element
+So, at the end we get the kth largest on top
+and least at the bottom
+
+// TC : O(nlogn + klogn) = O(nlogn)
+
+int findKthLargest(vector<int>& nums, int k) {
+    priority_queue<int> pq; // max heap
+
+    for (int num : nums) {
+        pq.push(num);
+    }
+
+    for (int i = 0; i < k - 1; i++) {
+        pq.pop();
     }
     return pq.top();
 }
