893b20777392a46b4571f0564ed39d1154414240
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-13 17:02:39 +0530

    Update Generate all binary strings.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 05.Recursion/2.Subsequences Pattern/Generate all binary strings.cpp
+++ 05.Recursion/2.Subsequences Pattern/Generate all binary strings.cpp
@@ -7,19 +7,15 @@
 // Input : K  = 4 
 // Output :0000 0001 0010 0100 0101 1000 1001 1010
 
-void generateBinaryStrings(int k, string str, vector<string> &result) {
-    // Base case: If the size of the string is equal to k, add it to the result
-    if (str.size() == k) {
-        result.push_back(str);
+void solve(int n, string str, vector<string>& ans) {
+
+    if (str.size() == n) {
+        ans.push_back(str);
         return;
     }
 
-    // If the string ends with '1', we can only add '0'
-    if (!str.empty() && str.back() == '1') {
-        generateBinaryStrings(k, str + "0", result);
-    } else {
-        // If the string ends with '0', we can add both '0' and '1'
-        generateBinaryStrings(k, str + "0", result);
-        generateBinaryStrings(k, str + "1", result);
-    }
+    solve(n, str + "1", ans);
+
+    if (str.empty() || str.back() == '1')
+        solve(n, str + "0", ans);
 }
