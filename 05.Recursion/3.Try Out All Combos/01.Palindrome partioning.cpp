5b678840b39679ddb5bd6be70c7a025ef7db476a
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-13 18:39:11 +0530

    Update 01.Palindrome partioning.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 05.Recursion/3.Try Out All Combos/01.Palindrome partioning.cpp
+++ 05.Recursion/3.Try Out All Combos/01.Palindrome partioning.cpp
@@ -27,36 +27,101 @@ Time Complexity: O(N * 2^N), where N is the length of the input string 's'. In t
 Space Complexity: O(N), where N is the length of the input string 's'. The space is used for storing the temporary partition vector and the vector of partitions.
 
 CODE:*/
-bool isPalindrome(string& palin){
-    int i = 0, j = palin.size() - 1;
-    while (i < j) {
-        if (palin[i] != palin[j])
-            return false;
-        i++;
-        j--;
-    }
-    return true;
-}
 
-void solve(int i, string& s, vector<string>& temp, vector<vector<string>>& ans) {
-    if (i >= s.size()) {
-        ans.push_back(temp);
-        return;
+class Solution {
+public:
+
+    vector<vector<bool>> precompute(string &s) {
+
+        int n = s.size();
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+
+                if (s[i] == s[j]) {
+                    if (j - i <= 2)             // if length is 1 or 2
+                        pal[i][j] = true;
+                    else
+                        pal[i][j] = pal[i + 1][j - 1];
+                }
+            }
+        }
+
+        return pal;
     }
 
-    for (int p = i; p < s.size(); p++) {
-        string palin = s.substr(i, p - i + 1);
-        if (isPalindrome(palin)) {
-            temp.push_back(palin);
-            solve(p + 1, s, temp, ans);
-            temp.pop_back();
+    void solve(int i, string &s, vector<string> &temp, vector<vector<string>> &ans,
+               vector<vector<bool>> &pal) {
+
+        if (i == s.size()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int j = i; j < s.size(); j++) {
+            if (pal[i][j]) {
+                temp.push_back(s.substr(i, j - i + 1));
+                solve(j + 1, s, temp, ans, pal);
+                temp.pop_back();
+            }
         }
     }
-}
 
-vector<vector<string>> partition(string s) {
-    vector<vector<string>> ans;
-    vector<string> temp;
-    solve(0, s, temp, ans);
-    return ans;
-}
+    vector<vector<string>> partition(string s) {2
+
+        vector<vector<string>> ans;
+        vector<string> temp;
+
+        vector<vector<bool>> pal = precompute(s);
+
+        solve(0, s, temp, ans, pal);
+
+        return ans;
+    }
+};
+
+// TC : O(N^2) + O(2^N) 
+// O(N^2) for precomputation and O(2^N) for take / ntake at each position
+// SC : O(N^2) + O(N) 
+// O(N^2) for DP table and O(N) for recursive stack
+
+// ___________________________________________________________________________________
+
+// SC : O(N) for recursive stack
+// TC : O(N 2^N) for take/ntake and isPalindrome check needs 'N'
+class Solution {
+public:
+
+    bool isPalindrome(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    void solve(int i, string &s, vector<string> &temp, vector<vector<string>> &ans) {
+        if (i == s.size()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int j = i; j < s.size(); j++) {
+            if (isPalindrome(s, i, j)) {
+                temp.push_back(s.substr(i, j - i + 1));
+                solve(j + 1, s, temp, ans);
+                temp.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(0, s, temp, ans);
+        return ans;
+    }
+};
