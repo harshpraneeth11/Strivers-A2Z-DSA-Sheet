dfe380ce58136861ed75531d6ffb604187d19411
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-18 21:03:29 +0530

    Update 05. Buy and sell stocks with cooldown.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/6. DP on Stocks/05. Buy and sell stocks with cooldown.cpp
+++ 14. Dynamic Programming/6. DP on Stocks/05. Buy and sell stocks with cooldown.cpp
@@ -30,12 +30,8 @@ CODE:
 // If you sell the stock, then be calm for 1 day.
 
 int getAns(vector<int> Arr, int ind, int buy, int n, vector<vector<int>> &dp) {
-    // Base case: If we've reached the end of the array, return 0 profit.
     if (ind >= n) return 0;
-    
-    // Check if the result is already computed
-    if (dp[ind][buy] != -1)
-        return dp[ind][buy];
+    if (dp[ind][buy] != -1) return dp[ind][buy];
         
     int profit;
     
@@ -60,60 +56,40 @@ int stockProfit(vector<int> &Arr) {
 }
 __________________________
 
+// Tabulation
 int stockProfit(vector<int> &Arr) {
     int n = Arr.size();
-    // Create a 2D DP array with dimensions (n+2) x 2, initialized to 0
     vector<vector<int>> dp(n + 2, vector<int>(2, 0));
-    
-    // Loop through the stock prices from the end to the beginning
+
     for (int ind = n - 1; ind >= 0; ind--) {
-        for (int buy = 0; buy <= 1; buy++) {
-            int profit;
 
-            if (buy == 0) { // We can buy the stock
-                profit = max(0 + dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1]);
-            }
+        // We can buy the stock
+        dp[ind][0] = max( dp[ind + 1][0], -Arr[ind] + dp[ind + 1][1] );
 
-            if (buy == 1) { // We can sell the stock
-                profit = max(0 + dp[ind + 1][1], Arr[ind] + dp[ind + 2][0]);
-            }
-
-            // Store the computed profit in the DP array
-            dp[ind][buy] = profit;
-        }
+        // We can sell the stock
+        dp[ind][1] = max( dp[ind + 1][1], Arr[ind] + dp[ind + 2][0] );
     }
 
     return dp[0][0];
 }
-__________________________
 
-int stockProfit(vector<int> &Arr) {
-    int n = Arr.size();
-    
-    // Initialize three arrays to track the maximum profit for buying and selling
-    vector<int> cur(2, 0);       // Current maximum profit
-    vector<int> front1(2, 0);    // Maximum profit one step ahead
-    vector<int> front2(2, 0);    // Maximum profit two steps ahead
-    
-    for (int ind = n - 1; ind >= 0; ind--) {
-        for (int buy = 0; buy <= 1; buy++) {
-            int profit;
+// _______________________________________________________________________
 
-            if (buy == 0) { // We can buy the stock
-                profit = max(0 + front1[0], -Arr[ind] + front1[1]);
-            }
+// Space optimized
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        vector<int> next(2, 0), next2(2, 0), curr(2, 0);
 
-            if (buy == 1) { // We can sell the stock
-                profit = max(0 + front1[1], Arr[ind] + front2[0]);
-            }
-            
-            cur[buy] = profit;
+        for (int i = prices.size() - 1; i >= 0; i--) {
+
+            curr[0] = max(next[0], -prices[i] + next[1]);
+            curr[1] = max(next[1],  prices[i] + next2[0]);
+
+            next2 = next;
+            next = curr;
         }
-        
-        // Update the 'front1' and 'front2' arrays with current values
-        front2 = front1;
-        front1 = cur;
+
+        return next[0];
     }
-    
-    return cur[0]; // Return the maximum profit for buying.
-}
+};
