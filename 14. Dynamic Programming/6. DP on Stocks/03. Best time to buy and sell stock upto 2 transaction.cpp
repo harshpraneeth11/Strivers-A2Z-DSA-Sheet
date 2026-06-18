cadb526084e3c473cc4c6d2ad7112539e4b758b9
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-18 14:53:33 +0530

    Update 03. Best time to buy and sell stock upto 2 transaction.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/6. DP on Stocks/03. Best time to buy and sell stock upto 2 transaction.cpp
+++ 14. Dynamic Programming/6. DP on Stocks/03. Best time to buy and sell stock upto 2 transaction.cpp
@@ -114,57 +114,53 @@ int maxProfit(vector<int>& prices, int n) {
 }
 ____________________
 
-// Tabulation
-int maxProfit(vector<int>& Arr, int n) {
-    // Creating a 3D DP array of size [n+1][2][3] initialized to 0
-    vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
 
-    // Base case: dp array is already initialized to 0, covering the base case.
+        int n = prices.size();
+        
+        // Creating a 3D DP array of size [n+1][2][3] initialized to 0
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(3, 0)));
+        // Base case: dp array is already initialized to 0, covering the base case.
 
-    for (int ind = n - 1; ind >= 0; ind--) {
-        for (int buy = 0; buy <= 1; buy++) {
+        for (int ind = n - 1; ind >= 0; ind--) {
             for (int cap = 1; cap <= 2; cap++) {
-                if (buy == 0) { // We can buy the stock
-                    dp[ind][buy][cap] = max(0 + dp[ind + 1][0][cap],
-                                           -Arr[ind] + dp[ind + 1][1][cap]);
-                }
+                // We can buy the stock
+                dp[ind][0][cap] = max( dp[ind + 1][0][cap],
+                    -prices[ind] + dp[ind + 1][1][cap] );
 
-                if (buy == 1) { // We can sell the stock
-                    dp[ind][buy][cap] = max(0 + dp[ind + 1][1][cap],
-                                           Arr[ind] + dp[ind + 1][0][cap - 1]);
-                }
+                // We can sell the stock
+                dp[ind][1][cap] = max( dp[ind + 1][1][cap],
+                    prices[ind] + dp[ind + 1][0][cap - 1] );
             }
         }
+        return dp[0][0][2];
     }
-
-    // The result is stored in dp[0][0][2] which represents maximum profit after the final transaction.
-    return dp[0][0][2];
-}
+};
 ____________________________
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
 
-// Space Optimization
-int maxProfit(vector<int>& Arr, int n) {
-    // Create two 2D arrays to store the profit information, one for the current state and one for the ahead state.
-    vector<vector<int>> ahead(2, vector<int>(3, 0));
-    vector<vector<int>> cur(2, vector<int>(3, 0));
+        int n = prices.size();
 
-    for (int ind = n - 1; ind >= 0; ind--) {
-        for (int buy = 0; buy <= 1; buy++) {
+        vector<vector<int>> ahead(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
+
+        for (int ind = n - 1; ind >= 0; ind--) {
             for (int cap = 1; cap <= 2; cap++) {
-                if (buy == 0) { // We can buy the stock
-                    cur[buy][cap] = max(0 + ahead[0][cap], 
-                                        -Arr[ind] + ahead[1][cap]);
-                }
 
-                if (buy == 1) { // We can sell the stock
-                    cur[buy][cap] = max(0 + ahead[1][cap],
-                                        Arr[ind] + ahead[0][cap - 1]);
-                }
+                // Buy
+                curr[0][cap] = max( ahead[0][cap],
+                    -prices[ind] + ahead[1][cap] );
+
+                // Sell
+                curr[1][cap] = max( ahead[1][cap],
+                    prices[ind] + ahead[0][cap - 1] );
             }
+            ahead = curr;
         }
-        // Update the ahead state with the current state for the next iteration.
-        ahead = cur;
+        return ahead[0][2];
     }
-
-    return ahead[0][2];
-}
+};
