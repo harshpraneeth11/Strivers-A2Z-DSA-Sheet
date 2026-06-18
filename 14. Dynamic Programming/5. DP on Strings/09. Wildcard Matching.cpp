e0c0add6a321aa12eb41d8834f0780bd6a8f45e5
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-18 20:55:07 +0530

    Update 09. Wildcard Matching.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 14. Dynamic Programming/5. DP on Strings/09. Wildcard Matching.cpp
+++ 14. Dynamic Programming/5. DP on Strings/09. Wildcard Matching.cpp
@@ -22,6 +22,15 @@ COMPLEXITY ANALYSIS:
 
 CODE:
 */
+/*
+Wild Card Matching : 
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+
+Regular Expression Matching :
+'.' Matches any single character
+'*' Matches zero or more of the preceding element
+*/
 
 bool allStars(int i, string& p) {
     for (int j = i; j >= 0; j--) {
@@ -54,3 +63,42 @@ bool isMatch(string s, string p) {
     vector<vector<int>> dp(n, vector<int>(m, -1));
     return fmemo(n - 1, m - 1, s, p, dp);
 }
+
+// ________________________________________________________
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
+        dp[0][0] = true;
+
+        // Pattern vs empty string
+        for (int j = 1; j <= m; j++) {
+            if (p[j - 1] == '*')
+                dp[0][j] = dp[0][j - 1];
+            else
+                break;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
