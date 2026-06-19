8016e77a0ad5f5aa1308e560ac5d024dba2490f1
harshpraneeth11 <harshapraneeth51@gmail.com>
2026-06-19 15:51:29 +0530

    Update 11.Set_matrix_0's.cpp
    Signed-off-by: harshpraneeth11 <harshapraneeth51@gmail.com>

--- 01.Arrays/2.Medium/11.Set_matrix_0's.cpp
+++ 01.Arrays/2.Medium/11.Set_matrix_0's.cpp
@@ -31,53 +31,41 @@ TC : O(N*M) and SC : O(1)
 
 */
 
-vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+// Using row0, col0 both code to be large
 
-    // int row[n] = {0}; --> matrix[..][0]
-    // int col[m] = {0}; --> matrix[0][..]
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-    int col0 = 1;
-    // step 1: Traverse the matrix and
-    // mark 1st row & col accordingly:
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == 0) {
-                // mark i-th row:
-                matrix[i][0] = 0;
+        int col0 = 1;
 
-                // mark j-th column:
-                if (j != 0)
+        // Mark rows and columns
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) col0 = 0;
+
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
                     matrix[0][j] = 0;
-                else
-                    col0 = 0;
-            }
-        }
-    }
-
-    // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j < m; j++) {
-            if (matrix[i][j] != 0) {
-                // check for col & row:
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
                 }
             }
         }
-    }
+        // Confusion is what if 1st row or 1st col or (0, 0) is zero
+        // If 1st col have a 0 or (0, 0) is 0, then col0 becomes 0
+        // If 1st row have a 0 and (0, 0) is not 0, then the row's left most element becomes 0, top most is already 0
 
-    //step 3: Finally mark the 1st col & then 1st row:
-    if (matrix[0][0] == 0) {
-        for (int j = 0; j < m; j++) {
-            matrix[0][j] = 0;
+
+        // Come from end, as we are using first row and col as markers
+        for(int i = m - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 1; j--) {       // only from n-1 to 1, not 0
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+
+            if(col0 == 0)
+                matrix[i][0] = 0;
         }
     }
-    if (col0 == 0) {
-        for (int i = 0; i < n; i++) {
-            matrix[i][0] = 0;
-        }
-    }
-
-    return matrix;
-}
-
+};
